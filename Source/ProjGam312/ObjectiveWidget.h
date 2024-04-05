a389c96b0b846d79a354b888992f57fb41fd36e2
LordMakelin <nrcabrera03@gmail.com>
2024-04-05 11:10:31 -0400

    Milestone 5 commit pt.2

---
+++ Source/ProjGam312/ObjectiveWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ObjectiveWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJGAM312_API UObjectiveWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void UpdateMatOBJ(float matsCollected);
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void UpdateBuildOBJ(float objectsBuilt);
+
+};
